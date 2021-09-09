92f7717126cb352c9468b2ef51a61605fb3fc824
Loic Coyle <loic.coyle@hotmail.fr>
2021-09-09 19:18:47 +0200

    tests: add rudimentary metric tests

---
+++ tests/unit/test_metrics.py
@@ -0,0 +1,35 @@
+from unittest import TestCase
+
+import numpy as np
+
+from phomo import metrics
+
+
+class TestMetrics(TestCase):
+    # TODO: these tests are not very good...
+    def test_norm(self):
+        a = np.ones((5, 4, 3))
+        b = np.ones((5, 4, 3))
+        assert metrics.norm(a, b) == 0
+
+        a = np.ones((5, 4, 3))
+        b = np.zeros((5, 4, 3))
+        assert metrics.norm(a, b) > 0
+
+    def test_greyscale(self):
+        a = np.ones((5, 4, 3))
+        b = np.ones((5, 4, 3))
+        assert metrics.greyscale(a, b) == 0
+
+        a = np.ones((5, 4, 3))
+        b = np.zeros((5, 4, 3))
+        assert metrics.greyscale(a, b) > 0
+
+    def test_luv_approx(self):
+        a = np.ones((5, 4, 3))
+        b = np.ones((5, 4, 3))
+        assert metrics.luv_approx(a, b) == 0
+
+        a = np.ones((5, 4, 3))
+        b = np.zeros((5, 4, 3))
+        assert metrics.luv_approx(a, b) > 0
