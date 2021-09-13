571a83dd68052907b5dacc51017ba9e14f2839d5
Loic Coyle <loic.coyle@hotmail.fr>
2021-09-13 22:21:22 +0200

    remove *args forwarding from metrics & doc strings cleanup

--- phomo/metrics.py
+++ phomo/metrics.py
@@ -1,6 +1,6 @@
 import sys
 
-# prior to python 3.8 the protocol is in typing_extensions
+# prior to python 3.8, Protocol is in typing_extensions
 if sys.version_info[0] == 3 and sys.version_info[1] < 8:
     from typing_extensions import Protocol
 else:
@@ -22,45 +22,47 @@ def register_metric(func):
 
 
 @register_metric
-def greyscale(img_a: np.ndarray, img_b: np.ndarray, *args, **kwargs) -> float:
+def greyscale(img_a: np.ndarray, img_b: np.ndarray, **kwargs) -> float:
     """Compute the greyscale distance.
 
     This metric ignores colours and compares greyscale values. Should provide better
     photomosaics when using few tiles images.
 
+
     Args:
         img_a: array containing the RGB pixels with values between 0 and 255.
         img_b: array containing the RGB pixels with values between 0 and 255.
-        *args, **kwargs: passed to `np.linalg.norm`.
+        **kwargs: passed to ``np.linalg.norm``.
 
     Returns:
         Colour distance approximation.
     """
     return np.linalg.norm(
-        np.subtract(img_a.sum(axis=-1), img_b.sum(axis=-1), dtype=float),
-        *args,
+        np.subtract(img_a.sum(axis=-1), img_b.sum(axis=-1), dtype=float).reshape(-1, 3),
         **kwargs,
     )
 
 
 @register_metric
-def norm(img_a: np.ndarray, img_b: np.ndarray, *args, **kwargs) -> float:
-    """`np.linalg.norm` distance metric.
+def norm(img_a: np.ndarray, img_b: np.ndarray, **kwargs) -> float:
+    """``np.linalg.norm`` distance metric.
 
     Quick distance metric in RGB space.
 
     Args:
         img_a: array containing the RGB pixels with values between 0 and 255.
         img_b: array containing the RGB pixels with values between 0 and 255.
-        *args, **kwargs: passed to `np.linalg.norm`.
+        **kwargs: passed to ``np.linalg.norm``.
 
     Returns:
         Colour distance approximation.
     """
-    return np.linalg.norm(np.subtract(img_a, img_b, dtype=float), *args, **kwargs)
+    return np.linalg.norm(
+        np.subtract(img_a, img_b, dtype=float).reshape(-1, 3), **kwargs
+    )
 
 
-# def norm(img_a: np.ndarray, img_b: np.ndarray, *args, **kwargs) -> float:
+# def norm(img_a: np.ndarray, img_b: np.ndarray, **kwargs) -> float:
 #     """`np.linalg.norm` distance metric.
 
 #     Args:
@@ -76,14 +78,14 @@ def norm(img_a: np.ndarray, img_b: np.ndarray, *args, **kwargs) -> float:
 #         np.linalg.norm(
 #             np.subtract(img_a, img_b, dtype=float),
 #             axis=-1,
-#             *args,
+#
 #             **kwargs,
 #         )
 #     )
 
 
 @register_metric
-def luv_approx(img_a: np.ndarray, img_b: np.ndarray, *args, **kwargs) -> float:
+def luv_approx(img_a: np.ndarray, img_b: np.ndarray, **kwargs) -> float:
     """Distance metric using a L*U*V space approximation.
 
     This metric should provide more accurate colour matching.
@@ -94,7 +96,7 @@ def luv_approx(img_a: np.ndarray, img_b: np.ndarray, *args, **kwargs) -> float:
     Args:
         img_a: array containing the RGB pixels with values between and 255.
         img_b: array containing the RGB pixels with values between and 255.
-        *args, **kwargs: passed to `np.linalg.norm`.
+        **kwargs: passed to ``np.linalg.norm``.
 
     Returns:
         Colour distance approximation.
@@ -102,9 +104,10 @@ def luv_approx(img_a: np.ndarray, img_b: np.ndarray, *args, **kwargs) -> float:
     r = (img_a[:, :, 0] + img_b[:, :, 0]) // 2
     d = np.subtract(img_a, img_b, dtype=float)
     return np.linalg.norm(
-        ((512 + r) * d[:, :, 0] ** 2)
-        + 1024 * d[:, :, 1] ** 2
-        + ((767 - r) * d[:, :, 2] ** 2),
-        *args,
+        (
+            ((512 + r) * d[:, :, 0] ** 2)
+            + 1024 * d[:, :, 1] ** 2
+            + ((767 - r) * d[:, :, 2] ** 2)
+        ).reshape(-1, 3),
         **kwargs,
     )
