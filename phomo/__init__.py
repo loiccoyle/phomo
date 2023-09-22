38c922b65a1a6b7922c08b346479bbaf2df1db6e
Loic Coyle <loic.coyle@hotmail.fr>
2023-09-23 00:45:22 +0200

    fix: add pkg __all__

--- phomo/__init__.py
+++ phomo/__init__.py
@@ -3,6 +3,9 @@ import logging
 from .master import Master
 from .mosaic import Mosaic
 from .pool import Pool
+from .metrics import METRICS
+
+__all__ = ["Master", "Mosaic", "Pool", "METRICS"]
 
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
